b727bbdf34a07027b376a985041e216ff91da622
Estefanía García Estévez <87451395+FaniGE@users.noreply.github.com>
2022-06-16 22:05:52 -0500

    Add files via upload

---
+++ Actividad14/peliculas.js
@@ -0,0 +1,57 @@
+const peliculas = [
+	{
+		nombre: 'el planeta del tesoro',
+		genero: 'Infantil', 
+		descripcion: 'Jim Hawkins, un chico rebelde a quien siempre le han apasionado las historias de piratas, compite para encontrar un tesoro en el espacio exterior.',
+		image: 'img/img1.jpg', 
+	},
+	{
+		nombre: 'como entrenar a tu dragon 2',
+		genero: 'Infantil', 
+		descripcion: 'Durante un viaje descubren una cueva cubierta de hielo que resulta ser el refugio de cientos de dragones salvajes.',
+		image: 'img/img2.jpg', 
+	},
+	{
+		nombre: 'sinbad',
+		genero: 'Infantil', 
+		descripcion: 'Simbad, el aventurero más osado del mundo, es acusado de robar uno de los tesoros más valiosos del planeta tierra, el poderoso Libro de la Paz. Ahora, debe recuperarlo si no quiere que su amigo Proteus muera.',
+		image: 'img/img3.jpg', 
+	},
+	{
+		nombre: 'matrix',
+		genero: 'Ciencia Ficción', 
+		descripcion: 'Una película es toda obra audiovisual fijada en cualquier medio o soporte, cuyo desarrollo se define como un trabajo de creación, producción, montaje y postproducción.',
+		image: 'img/img4.jpg' 
+	},
+	{
+		nombre: 'alien',
+		genero: 'Ciencia Ficción', 
+		descripcion: 'Una película es toda obra audiovisual fijada en cualquier medio o soporte, cuyo desarrollo se define como un trabajo de creación, producción, montaje y postproducción.',
+		image: 'img/img5.jpg' 
+	},
+	{
+		nombre: 'avatar',
+		genero: 'Ciencia Ficción', 
+		descripcion: 'Una película es toda obra audiovisual fijada en cualquier medio o soporte, cuyo desarrollo se define como un trabajo de creación, producción, montaje y postproducción.',
+		image: 'img/img6.jpg' 
+	},
+	{
+		nombre: 'fear street',
+		genero: 'Terror', 
+		descripcion: 'Una película es toda obra audiovisual fijada en cualquier medio o soporte, cuyo desarrollo se define como un trabajo de creación, producción, montaje y postproducción.',
+		image: 'img/img7.png', 
+	},
+    {
+		nombre: 'the witch',
+		genero: 'Terror', 
+		descripcion: 'Una película es toda obra audiovisual fijada en cualquier medio o soporte, cuyo desarrollo se define como un trabajo de creación, producción, montaje y postproducción.',
+		image: 'img/img8.jpg', 
+	},
+    {
+		nombre: 'insidious',
+		genero: 'Terror', 
+		descripcion: 'Una película es toda obra audiovisual fijada en cualquier medio o soporte, cuyo desarrollo se define como un trabajo de creación, producción, montaje y postproducción.',
+		image: 'img/img9.jpg', 
+	},
+    
+];
